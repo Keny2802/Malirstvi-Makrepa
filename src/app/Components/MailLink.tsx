980a05b112e3d4b6aa25a68e2b83ab76833d3af5
Keny2802 <oliva.vojta@gmail.com>
2026-02-14 18:10:45 +0100

    Změna EMAILU v kontaktu.

--- src/app/Components/MailLink.tsx
+++ src/app/Components/MailLink.tsx
@@ -19,10 +19,12 @@ const MailLink = ({ ...props }: mailLinkType) => {
     return (
         <Fragment>
             <Link
-                href="mailto:info@malirstvi-ostrava.cz"
+                // href="mailto:info@malirstvi-ostrava.cz"
+                href="mailto:joskre@centrum.cz"
                 className={clsx(`${className || ""} text-base flex items-center gap-1.5 md:gap-2 lg:gap-2.5 transition-colors duration-300 ease-in-out hover:text-[#1d4ed8]`)}>
                 {children}
-                info@malirstvi-ostrava.cz
+                {/* info@malirstvi-ostrava.cz */}
+                joskre@centrum.cz
             </Link>
         </Fragment>
     );
